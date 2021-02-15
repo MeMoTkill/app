2eb37ba60c17c75ee0f7dc7239275f6e8bea6547
MeMoTkill <gataullinranel@gmail.com>
2021-02-15 22:08:06 +0300

    asd

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -10,33 +10,26 @@ import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
 import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
 
-import Tabbar from '@vkontakte/vkui/dist/components/Tabbar/Tabbar';//Кнопочки внизу экрана
-
 const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
-		<PanelHeader>Пример теструю</PanelHeader>
-		
+		<PanelHeader>Example</PanelHeader>
+		{fetchedUser &&
+		<Group header={<Header mode="secondary">User Data Fetched with VK Bridge</Header>}>
+			<Cell
+				before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
+				description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
+			>
+				{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
+			</Cell>
+		</Group>}
 
-		<Group header={<Header mode="secondary">Кнопачкилар</Header>}>
+		<Group header={<Header mode="secondary">Navigation Example</Header>}>
 			<Div>
 				<Button stretched size="l" mode="secondary" onClick={go} data-to="persik">
-					Покажи мне персика
+					Show me the Persik, please
 				</Button>
 			</Div>
 		</Group>
-
-		<Tabbar>
-      <TabbarItem text="Новости">
-        <Icon28NewsfeedOutline />
-      </TabbarItem>
-      <TabbarItem indicator={<Counter size="s" mode="prominent">12</Counter>} text="Сообщения">
-        <Icon28MessageOutline />
-      </TabbarItem>
-      <TabbarItem indicator={<Badge mode="prominent" />} text="Клипы">
-        <Icon28ClipOutline />
-      </TabbarItem>
-    </Tabbar>
-
 	</Panel>
 );
 
